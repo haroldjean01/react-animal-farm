f76f35d6da78fe1c74316cc00efb6a8f76d596f6
Harold Jean <haroldjean38@yahoo.com>
2023-01-09 18:27:27 -0500

    rendered name, type and says

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,10 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="cat-container" style={{ top: props.top, left: props.left }}>
+      {/* <img src="benny.png" /> */}
+      <img src={props.image} />
+      <h2>{props.name}</h2>
+      <p>{props.says}</p>
+    </div>
+  );
 }
